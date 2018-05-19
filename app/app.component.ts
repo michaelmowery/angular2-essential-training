75451e8ff176091aec70801f55da954f5fc91f87
Michael Mowery <michael@telluridetech.com>
2018-05-19 15:18:10 -0600

    follow along

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['app/app.component.css']
 })
 export class AppComponent {
+  onMediaItemDelete(mediaItem) {}
+
   firstMediaItem = {
     id: 1,
     name: "Firebug",
